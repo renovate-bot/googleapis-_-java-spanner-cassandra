fb60ad1e859668a1394ff9197e28577a320bc995
Atharva Moroney <moroneyatharva@gmail.com>
2025-07-12 23:37:16 -0700

    chore: Fix Cassandra IT (#153)

--- integration-tests/src/test/java/com/google/cloud/spanner/adapter/utils/CassandraContext.java
+++ integration-tests/src/test/java/com/google/cloud/spanner/adapter/utils/CassandraContext.java
@@ -24,6 +24,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import org.testcontainers.cassandra.CassandraContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.utility.DockerImageName;
 
 /**
@@ -35,11 +36,18 @@ import org.testcontainers.utility.DockerImageName;
 public class CassandraContext extends DatabaseContext {
   private static final DockerImageName CASSANDRA_IMAGE = DockerImageName.parse("cassandra:5.0.4");
   private static final int PORT = 9042;
-
   private CqlSession session;
-
   private static final CassandraContainer cassandraContainer =
-      new CassandraContainer(CASSANDRA_IMAGE).withExposedPorts(PORT);
+      new CassandraContainer(CASSANDRA_IMAGE)
+          .withExposedPorts(PORT)
+          .waitingFor(Wait.forLogMessage(".*Starting listening for CQL clients.*\\n", 1))
+          .withStartupTimeout(Duration.ofMinutes(3));
+
+  static {
+    // Start only one container for all test classes. It will be closed automatically when JVM
+    // exits.
+    cassandraContainer.start();
+  }
 
   public CassandraContext() {
     super("Cassandra");
@@ -47,34 +55,27 @@ public class CassandraContext extends DatabaseContext {
 
   @Override
   public void initialize() throws Exception {
-    try {
-      cassandraContainer.start();
-      session =
-          CqlSession.builder()
-              .addContactPoint(cassandraContainer.getContactPoint())
-              .withLocalDatacenter(cassandraContainer.getLocalDatacenter())
-              .withConfigLoader(
-                  DriverConfigLoader.programmaticBuilder()
-                      .withString(DefaultDriverOption.PROTOCOL_VERSION, "V4")
-                      .withDuration(DefaultDriverOption.REQUEST_TIMEOUT, Duration.ofMinutes(5))
-                      .withDuration(
-                          DefaultDriverOption.CONNECTION_INIT_QUERY_TIMEOUT, Duration.ofMinutes(5))
-                      .build())
-              .build();
+    session =
+        CqlSession.builder()
+            .addContactPoint(cassandraContainer.getContactPoint())
+            .withLocalDatacenter(cassandraContainer.getLocalDatacenter())
+            .withConfigLoader(
+                DriverConfigLoader.programmaticBuilder()
+                    .withString(DefaultDriverOption.PROTOCOL_VERSION, "V4")
+                    .withBoolean(DefaultDriverOption.REQUEST_WARN_IF_SET_KEYSPACE, false)
+                    .withDuration(DefaultDriverOption.REQUEST_TIMEOUT, Duration.ofMinutes(5))
+                    .withDuration(
+                        DefaultDriverOption.CONNECTION_INIT_QUERY_TIMEOUT, Duration.ofMinutes(5))
+                    .build())
+            .build();
 
-      String createKeyspaceCql =
-          String.format(
-              "CREATE KEYSPACE IF NOT EXISTS %s WITH REPLICATION = { 'class' : 'SimpleStrategy',"
-                  + " 'replication_factor' : 1 };",
-              keyspace());
-      session.execute(createKeyspaceCql);
-      session.execute("USE " + keyspace());
-    } catch (Exception e) {
-      if (cassandraContainer != null && cassandraContainer.isRunning()) {
-        cassandraContainer.stop();
-      }
-      throw e;
-    }
+    String createKeyspaceCql =
+        String.format(
+            "CREATE KEYSPACE IF NOT EXISTS %s WITH REPLICATION = { 'class' : 'SimpleStrategy',"
+                + " 'replication_factor' : 1 };",
+            keyspace());
+    session.execute(createKeyspaceCql);
+    session.execute("USE " + keyspace());
   }
 
   @Override
@@ -97,13 +98,8 @@ public class CassandraContext extends DatabaseContext {
 
   @Override
   public void cleanup() throws Exception {
-    try {
-      if (session != null && !session.isClosed()) {
-        session.close();
-      }
-    } finally {
-      // Do nothing.
-      // JVM should stop the container upon existing.
+    if (session != null && !session.isClosed()) {
+      session.close();
     }
   }
 
